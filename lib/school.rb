ea81b84dafc6311c672ee692e7fa48ad46ab6750
Brandon Yoon <byoon5397@gmail.com>
2020-04-01 14:30:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = []
+    if @roster != nil
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+
+
+
+end
