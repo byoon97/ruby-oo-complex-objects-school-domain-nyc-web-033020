d86dd1e9b1833e946ef9c29a340b5a5305370111
Brandon Yoon <byoon5397@gmail.com>
2020-04-01 14:41:52 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    @roster.sort_by do |grade, names|
+      grade
+    end
+  end
+
+
+
+
+end
